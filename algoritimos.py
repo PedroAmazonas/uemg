fcdd4d18d21337a814dc1d03e0fb5c843a625f40
unknown <pedroamazonas09@gmail.com>
2024-07-14 17:49:29 -0300

    Funcao Barra

--- algoritimos.py
+++ algoritimos.py
@@ -87,25 +87,26 @@ def funcao(x):
 		t.goto(x,x*10)
 	t.clear()	
 #funcao(40)
+def Barra(t,heigth):
+	def drawBar(t, height):
+		""" Get turtle t to draw one bar, of height. """
+		t.begin_fill()               # start filling this shape
+		t.left(90)
+		t.forward(height)
+		t.write(str(height))
+		t.right(90)
+		t.forward(40)
+		t.right(90)
+		t.forward(height)
+		t.left(90)
+		t.end_fill()                 # stop filling this shape
 
-def drawBar(t, height):
-    """ Get turtle t to draw one bar, of height. """
-    t.begin_fill()               # start filling this shape
-    t.left(90)
-    t.forward(height)
-    t.write(str(height))
-    t.right(90)
-    t.forward(40)
-    t.right(90)
-    t.forward(height)
-    t.left(90)
-    t.end_fill()                 # stop filling this shape
-
-xs = [88,48, 117, 200, 240, 160, 260, 220]  #
-maxheight = max(xs)
-numbars = len(xs)
-border = 10
-ts.setworldcoordinates(0-border, 0-border, 40*numbars+border, maxheight+border)
-for a in xs:
-    drawBar(t, a)
+	xs = [88,48, 117, 200, 240, 160, 260, 220]  #
+	maxheight = max(xs)
+	numbars = len(xs)
+	border = 10
+	ts.setworldcoordinates(0-border, 0-border, 40*numbars+border, maxheight+border)
+	for a in xs:
+		drawBar(t, a)
+Barra(t,25)		
 ts.exitonclick()	
